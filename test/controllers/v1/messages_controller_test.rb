674b37eaa07888509b125c3a5f420c6d768c794f
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-07-04 18:13:58 -0300

    fix linters

--- test/controllers/v1/messages_controller_test.rb
+++ test/controllers/v1/messages_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class V1::MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get v1_messages_index_url
     assert_response :success
   end
